e9f976e2e69748a07bc6b45a7a36c20bf8c9557c
DanielFloresDev17 <danielflores17@gmail.com>
2024-01-21 20:40:39 -0500

    BugFixes

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -47,6 +47,6 @@ class RegisterController extends Controller
         auth()->attempt($request->only('email', 'password'));
 
         //Redireccionar
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 }
